11af41e69be02be3b60f1f0fe2d7b295dad5661d
Muhammad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2023-05-18 12:00:26 +0700

    test(chapter2/calculator): add addition with negative number

--- chapter2/calculator/engine_test.go
+++ chapter2/calculator/engine_test.go
@@ -7,10 +7,31 @@ import (
 )
 
 func TestAdd(t *testing.T) {
+	ce := calculator.Engine{}
+
+	actAssert := func(x, y, want float64) {
+		// act
+		got := ce.Add(x, y)
+
+		// Assert
+		require.Equal(t, want, got)
+	}
+
 	t.Run("success", func(t *testing.T) {
+		// Arrange
 		expectedResult := 2.0
-		ce := calculator.Engine{}
+
+		// Act
 		res := ce.Add(1.0, 1.0)
+
+		// Assert
 		require.Equal(t, expectedResult, res)
+	})
+
+	t.Run("negative number", func(t *testing.T) {
+		x := -1.0
+		y := -2.0
+
+		actAssert(x, y, -3.0)
 	})
 }
