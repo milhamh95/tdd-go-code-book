46f5bca86f9287000ab08bb4e94ba074a21049f0
Muhammad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2023-05-18 11:53:44 +0700

    test(chapter2/calculator): refactor add expected result variable

--- chapter2/calculator/engine_test.go
+++ chapter2/calculator/engine_test.go
@@ -8,7 +8,8 @@ import (
 )
 
 func TestAdd(t *testing.T) {
+	expectedResult := 2.0
 	ce := calculator.Engine{}
 	res := ce.Add(1.0, 1.0)
-	require.Equal(t, 2.0, res)
+	require.Equal(t, expectedResult, res)
 }
