75f109d4f8a4b1459796648faca5e7b3c537b190
Muhammad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2023-05-18 11:55:06 +0700

    test(chapter2/calculator): refactor addition success case

--- chapter2/calculator/engine_test.go
+++ chapter2/calculator/engine_test.go
@@ -1,15 +1,16 @@
 package calculator_test
 
 import (
+	"github.com/stretchr/testify/require"
 	"tdd-go-code-book/chapter2/calculator"
 	"testing"
-
-	"github.com/stretchr/testify/require"
 )
 
 func TestAdd(t *testing.T) {
-	expectedResult := 2.0
-	ce := calculator.Engine{}
-	res := ce.Add(1.0, 1.0)
-	require.Equal(t, expectedResult, res)
+	t.Run("success", func(t *testing.T) {
+		expectedResult := 2.0
+		ce := calculator.Engine{}
+		res := ce.Add(1.0, 1.0)
+		require.Equal(t, expectedResult, res)
+	})
 }
