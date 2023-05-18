608c6ddee1bc79cf78aff8b5b13a935c2e550de8
Muhammad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2023-05-18 09:40:38 +0700

    feat(calculator): implement addition logic

--- chapter1/calculator/calculator.go
+++ chapter1/calculator/calculator.go
@@ -1,5 +1,6 @@
 package calculator
 
 func Addition(x, y int) (int, error) {
-	return 0, nil
+	res := x + y
+	return res, nil
 }
--- chapter1/calculator/calculator_test.go
+++ chapter1/calculator/calculator_test.go
@@ -3,11 +3,13 @@ package calculator_test
 import (
 	"testing"
 
+	"tdd-go-code-book/chapter1/calculator"
+
 	"github.com/stretchr/testify/require"
 )
 
 func Test_Addition(t *testing.T) {
 	res, err := calculator.Addition(1, 2)
 	require.Equal(t, 3, res)
-	require.NoError(err)
+	require.NoError(t, err)
 }
