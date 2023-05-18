e42d28681bab40e177568a931601802f643bfc12
Muhammad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2023-05-18 21:03:31 +0700

    test(chapter2/calculator): assert error string value and expression

--- chapter2/format/error_test.go
+++ chapter2/format/error_test.go
@@ -8,10 +8,14 @@ import (
 )
 
 func TestError(t *testing.T) {
+	// Arrange
 	expectedErr := errors.New("error msg")
 	expr := "2%3"
+
+	// Act
 	wrappedErr := format.Error(expr, expectedErr)
 
+	// Assert.
 	require.NotNil(t, wrappedErr)
 	require.ErrorContains(t, wrappedErr, expectedErr.Error())
 	require.ErrorContains(t, wrappedErr, expr)
