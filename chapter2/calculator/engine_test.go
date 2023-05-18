d4e4d9503942bc216d83e00f0f33d721c6e1c3fc
Muhammad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2023-05-18 10:38:40 +0700

    test(chapter2/calculator): implement test for add calculator

--- chapter2/calculator/engine_test.go
+++ chapter2/calculator/engine_test.go
@@ -1,7 +1,12 @@
 package calculator_test
 
-import "testing"
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
 
 func TestAdd(t *testing.T) {
+	res := calculator.Add(1, 1)
 
+	require.Equal(t, 2, res)
 }
